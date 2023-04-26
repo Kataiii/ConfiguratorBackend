932748ef5b87a49d95845a1f3f26ad5a4087c6db
katya <katya_yuneva@mail.ru>
2023-04-26 12:33:55 +0400

    add connection database

--- src/app.service.ts
+++
@@ -1,9 +0,0 @@
-//Сервисы содержат логику и могут переиспользоваться в других компонентах
-import { Injectable } from "@nestjs/common";
-
-@Injectable()
-export class AppService{
-    getUsers(){
-        return [{id:1, name:'Qwerty'}]
-    }
-}
